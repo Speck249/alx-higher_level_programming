17f2a607e0b49c0b9d3ea1fae347fb7bbfacd334
rediet <redietkfle@gmail.com>
2023-03-09 14:41:55 +0000

    Print sum of Command Line Args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv)
+    result = 0
+    for i in range(n - 1):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
