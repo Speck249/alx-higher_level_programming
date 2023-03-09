a3a9d8b44cfa8458ad9086c66c718ded0c6d47d1
rediet <redietkfle@gmail.com>
2023-03-09 13:31:05 +0000

    Print argc & argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-if __name__ == "__main__":   
+if __name__ == "__main__":
     import sys
-    i = len(sys.argv) - 1
-    if i == 0 and i == 1:
-        print("{} arguments.".format(i))
+    x = len(sys.argv) - 1
 
-    elif i > 0:
-        print("{} arguments:".format(i))
-        i = 0
+    if x == 0:
+        print("{} arguments.".format(x))
+    elif x == 1:
+        print("{} argument:".format(x))
+    else:
+        print("{} arguments:".format(x))
+
+    if x > 0:
+        x = 0
         for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i = i + 1
+            if x > 0:
+                print("{}: {}".format(x, arg))
+            x = x + 1
