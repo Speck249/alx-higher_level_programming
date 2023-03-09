307f068b9741f6548be57b5e1962a35dc5b8a11d
rediet <redietkfle@gmail.com>
2023-03-09 09:39:32 +0000

    Import add function from module

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#Python Addition Module
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return a + b;
