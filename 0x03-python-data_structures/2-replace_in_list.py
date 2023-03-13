8a90c32e936957ce854f9b85d396e503f093d3eb
rediet <redietkfle@gmail.com>
2023-03-13 07:46:33 +0000

    Replace an element in a list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    
+    n = len(my_list)
+
+    elif idx >= n:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
