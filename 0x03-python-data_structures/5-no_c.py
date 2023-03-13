d613cdfc448ba76d57c471ab79c7334534a763f6
rediet <redietkfle@gmail.com>
2023-03-13 08:20:12 +0000

    Remove chars from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-   new_string = my_string[:]
-   bad_char = ['c', 'C']
-   new_string = ''.join(i for i in my_string if not i in bad_char)
-   return new_string
+    new_string = my_string[:]
+    bad_char = ['c', 'C']
+    new_string = ''.join(i for i in my_string if not i in bad_char)
+    return new_string
