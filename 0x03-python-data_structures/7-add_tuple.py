21c95b00d14e848d3ee86b13b953da07ac0e480b
rediet <redietkfle@gmail.com>
2023-03-13 12:37:01 +0000

    Sum of two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = len(tuple_a)
+    b = len(tuple_b)
+
+    if a == 0:
+        tuple_a = 0, 0
+
+    elif a == 1:
+        tuple_a = tuple_a[0], 0
+
+    else:
+        tuple_a = tuple_a[0], tuple_a[1]
+
+    if b == 0:
+        tuple_b = 0, 0
+
+    elif b == 1:
+        tuple_b = tuple_b[0], 0
+
+    else:
+        tuple_b = tuple_b[0], tuple_b[1]
+
+    result = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return result
