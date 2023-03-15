7cdb46bc6881c97c306a02436913dfbf2d2f1620
rediet <redietkfle@gmail.com>
2023-03-15 12:26:43 +0000

    Return set of elements present in only one set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    result = [i for i in set_1 if i not in set_2] + [j for j in set_2 if j not in set_1]
+    result = [i for i in set_1 if i not in set_2] +
+    [j for j in set_2 if j not in set_1]
     return result
