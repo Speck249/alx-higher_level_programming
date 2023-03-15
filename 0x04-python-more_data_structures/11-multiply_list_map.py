65e212926749c56e497f59cf65b2e95a99cb6f30
rediet <redietkfle@gmail.com>
2023-03-15 12:45:50 +0000

    Return list of integers multiplied by number

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
-    result = list(map(lambda i: i * number, new_list))
-    return result
+    new_list = list(map(lambda i: i * number, new_list))
