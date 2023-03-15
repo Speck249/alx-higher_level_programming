1718a2a59e90e5792888b5e35c37b24229567722
rediet <redietkfle@gmail.com>
2023-03-15 12:23:13 +0000

    Return set of common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    n = len(set_1)
-    m = len(set_2)
-    if n > 0 and m > 0:
-        result = [i for i in set_1 if i in set_2]
-        return result
+    result = [i for i in set_1 if i in set_2]
+    return result
