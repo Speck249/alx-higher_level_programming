908e1cfff1c8b427846010241323813dd26858bd
rediet <redietkfle@gmail.com>
2023-03-16 17:29:44 +0000

    Delete keys with specific value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,9 +4,9 @@ def complex_delete(a_dictionary, value):
     new_dict = []
 
     for i in a_dictionary:
-        if a_dictionary[key] == value:
+        if a_dictionary[i] == value:
             new_dict.append(i)
 
     for j in new_dict:
-        del a_dictionary[key]
+        del a_dictionary[j]
     return a_dictionary
