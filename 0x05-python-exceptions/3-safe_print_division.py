995937dc9665fb07372ed0be27d1f7a66287c3da
rediet <redietkfle@gmail.com>
2023-03-27 13:57:55 +0000

    Return result of division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    else:
+        return result
+    finally:
+        if b != 0:        
+            print("Inside result: {}".format(result))
+        else:
+            print("Inside result: None")
