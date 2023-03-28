1af316f17d59de071d4ce184491cd7b3593a3944
rediet <redietkfle@gmail.com>
2023-03-28 12:23:29 +0000

    Print square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+"""Python Classes and Objects."""
 
 class Square:
-    i = j = 0
+    """Creates an empty class Square which defines a square."""
+    i - j = 0
     def __init__(self, size=0):
+        """The init method instantiates the empty class
+        with a new object.
+        Args:
+            size = size of the square.
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if the value of size is negative.
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -10,14 +21,23 @@ class Square:
         self.__size = size
 
     def area(self):
-        ar = self.__size ** 2
+        """Computes the area of the square.
+        Prints result.
+        """
 
+        ar = self.__size ** 2
+        return ar
+ 
     @property
     def size(self):
+        """Gets value of size object."""
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Sets the value of size object."""
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -25,6 +45,9 @@ class Square:
         self.__size = value
 
     def my_print(self):
+       """If value of size is positive, method prints
+       a square."""
+
        if self.__size < 0:
            print()
        else:
