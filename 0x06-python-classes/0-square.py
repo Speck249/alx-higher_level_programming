83a71f3d256609eafce1db8cd625475ef5cd3258
rediet <redietkfle@gmail.com>
2023-03-28 11:39:42 +0000

    Create an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,7 +2,5 @@
 """Class defines square."""
 
 class Square:
-    """Statement creates an empty class
-    named Squared without body.
-    """
+    """Statement creates an empty class."""
     pass
