739d93303607bd4ec906b2851682987f85282f77
rediet <redietkfle@gmail.com>
2023-03-28 11:08:16 +0000

    Instantiate empty class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Class defines a square."""
 
 class Square:
+    """A new class, Square, is created.
+    The init method then instantiates an object,
+    size, in the class Square.
+    """
+
     def __init__(self, size):
         self.__size = size
