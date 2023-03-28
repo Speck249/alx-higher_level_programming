310652b244466b575e993b8308508c7a1a1587b0
rediet <redietkfle@gmail.com>
2023-03-28 13:36:25 +0000

    Access & update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,7 +6,7 @@ class Square:
     """Creates a class."""
 
     def __init__(self, size=0):
-        """Instantiates new class.
+        """Instantiates the new class.
         Args:
             size: size of the square.
         Raises:
@@ -15,36 +15,33 @@ class Square:
         """
 
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
 
         self.__size = size
 
+    @property
+    def size(self):
+        """Retrieves size of square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets size of square."""
+
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
     def area(self):
         """
-        Computes area of square from given size.
-        Returns: area of square.
+        Calculate area of the square from given size.
+        Returns: area of the square.
         """
 
         result = self.__size ** 2
         return result
-
- 
-    @property
-    def size(self):
-        """Method retrieves value of size."""
-
-        return self.__size
-
-
-    @size.setter
-    def size(self, value):
-        """Method sets value of size."""
-
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = value
