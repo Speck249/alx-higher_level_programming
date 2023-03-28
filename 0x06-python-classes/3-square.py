5c57fc7af0986402c90fcc4afd782fb4cd637705
rediet <redietkfle@gmail.com>
2023-03-28 13:11:09 +0000

    Compute area of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -18,10 +18,12 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
 
     def area(self):
-        """Computes area of square from given size.
+        """
+        Computes area of square from given size.
         Returns: area of square.
         """
 
