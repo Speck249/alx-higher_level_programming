cae56c9a0c63ee22a75e0712207be1594828f952
rediet <redietkfle@gmail.com>
2023-03-28 17:58:32 +0000

    Print square on specific coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,7 +49,7 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
         if len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if value[0] < 0 and value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
