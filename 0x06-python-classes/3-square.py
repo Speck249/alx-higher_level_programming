a3afecc7984bf15bf0b1eea9f8ebc11bf77dad54
rediet <redietkfle@gmail.com>
2023-03-28 12:10:07 +0000

    Print area of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""Python Classes and Objects."""
 
 class Square:
+    """Creates en empty class Square which defines a square."""
+
     def __init__(self, size=0):
+        """The init method instantiates the empty class
+        with a new object.
+        Args:
+            size = size of the square.
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if the value of size is negative.
+        """
+
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -9,5 +21,9 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
-        ar = self.__size ** 2
-        return ar
+        """Computes the area of the square.
+        Prints result.
+        """
+
+        result = self.__size ** 2
+        return result
