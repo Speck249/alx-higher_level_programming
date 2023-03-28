53d5a93442ad41754713c4b9371654a303b044e6
rediet <redietkfle@gmail.com>
2023-03-28 13:24:12 +0000

    Access & update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -38,8 +38,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """Method sets value of size."""
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
