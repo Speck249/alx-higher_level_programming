7b2f5a555ecda408bec2da0f9ec06d2f0eac2bd2
rediet <redietkfle@gmail.com>
2023-04-03 14:03:48 +0000

    Instantiate new class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""Module creates class."""
+"""Module instantiates class."""
 
 
 class Rectangle:
-    """Creates class."""
+    """Creates a class."""
 
-    def __init__(self, height=0, width=0):
-    """Instantiates new class.
+    def __init__(self, width=0, height=0):
+    """Instantiates the new class.
     Args:
         width - width of rectangle.
         height - height of rectangle.
+    Raises:
+        TypeError: if width or height are not integers.
+        ValueError: if width or height are less than zero.
     """
 
-    self.__height = height
-    self.__width = width
+    self.width = width
+    self.height = height
     
     @property
     def width(self):
-    """Retrieves width of rectangle."""
+        """Method retrieves width of rectangle."""
 
         return self.__width
 
     @width.setter
     def width(self, value):
-    """Sets width of rectangle."""
+        """Method sets width of rectangle."""
     
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
@@ -33,13 +36,13 @@ class Rectangle:
 
     @property
     def height(self):
-    """Retrieves height of rectangle."""
+        """Method retrieves height of rectangle."""
     
         return self.__height
     
     @height.setter
     def height(self, value):
-    """Sets width of rectangle."""
+        """Method sets height of rectangle."""
     
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
