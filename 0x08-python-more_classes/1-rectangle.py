a9ce281c0fd91711cbdd5ae810265477a9f8aed0
rediet <redietkfle@gmail.com>
2023-04-03 14:13:40 +0000

    Instantiates new class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,51 +1,50 @@
 #!/usr/bin/python3
-"""Module instantiates class."""
+"""Module instantiates new class."""
 
 
 class Rectangle:
     """Creates a class."""
 
     def __init__(self, width=0, height=0):
-    """Instantiates the new class.
-    Args:
-        width - width of rectangle.
-        height - height of rectangle.
-    Raises:
-        TypeError: if width or height are not integers.
-        ValueError: if width or height are less than zero.
-    """
+        """Instantiates the new class.
+        Args:
+            width: width of the rectangle.
+            height: height of the rectangle.
+        Raises:
+            TypeError: if width and height are not an integer.
+            ValueError: if width and height are less than 0.
+        """
+        self.__width= width
+        self.__height = height
 
-    self.width = width
-    self.height = height
-    
     @property
     def width(self):
-        """Method retrieves width of rectangle."""
+        """Method retrieves width of rectangle"""
 
         return self.__width
 
     @width.setter
     def width(self, value):
         """Method sets width of rectangle."""
-    
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
+
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
     def height(self):
-        """Method retrieves height of rectangle."""
-    
+        """Method retrieves height of rectangle"""
+
         return self.__height
-    
-    @height.setter
+
+    @width.setter
     def height(self, value):
         """Method sets height of rectangle."""
-    
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
         self.__height = value
