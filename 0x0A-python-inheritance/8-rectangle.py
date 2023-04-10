9ff860ca28dfa40549dbe2379d58f8c1fa666b55
rediet <redietkfle@gmail.com>
2023-04-10 12:26:43 +0000

    Instantiate class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module inherits from Base Geometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class inherits from Base Geometry."""
+
+    def __init__(self, width, height):
+        """Method instantiates class."""
+
+        self.__width = width
+        self.integer_validator("width", width)
+        self.__height = height
+        self.integer_validator("height", height)
