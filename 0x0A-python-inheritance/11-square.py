9104e379c88d4ca6582bbf83090257e1c077f811
rediet <redietkfle@gmail.com>
2023-04-10 17:39:36 +0000

    Return area & square description

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,13 +5,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Class inherits from Rectangle."""
-    
+ 
     def __init__(self, size):
         """Method instantiates class."""
-        
+ 
         self.integer_validator("size", size)
         """Validates value of size."""
-        
+ 
         super().__init__(size, size)
         self.__size = size
         """Sets private class instance."""
@@ -20,10 +20,10 @@ class Square(Rectangle):
         """Computes area of square.
         Returns: area of square.
         """
-        
+ 
         return super().area()
 
     def __str__(self):
         """Dunder method returns string representation."""
-        
+ 
         return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
