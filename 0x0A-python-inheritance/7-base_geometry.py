8b18f799709b71ba4a04585104623c237f8037b4
rediet <redietkfle@gmail.com>
2023-04-10 12:18:41 +0000

    Validate value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""Module creates empty class."""
+"""Module presents method that validates value."""
 
 
 class BaseGeometry:
     """Creates an empty class."""
 
     def area(self):
-        """Function raises an exception.
-
-        Args:
-            passed self to instance method.
-
-        Returns:
-            no return value.
-
-        Raises:
-            a custom exception message.
-        """
+        """Class method not implemented."""
 
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Function validates value.
+        """Method validates value.
 
         Args:
-            name: first parameter
-            value: second parameter
+            name: name of value
+            value: to be validated
  
         Returns:
             no return value
@@ -35,7 +25,7 @@ class BaseGeometry:
             ValueError: if value <= 0
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
