71d17fc36c176de30fcd6e9706eb30d3ac7d5b4a
rediet <redietkfle@gmail.com>
2023-04-10 17:55:34 +0000

    Create class that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Module creates class that inherits from int."""
 
+
 class MyInt(int):
     """Creates class that inherits from int."""
+
 
     def __eq__(self, other):
         """Runs negation check."""
         return int.__ne__(self, other)
+
 
     def __ne__(self, other):
         """Runs equality check."""
