76d2cada807f021dbc7b8a338af8174f0c70f8a0
rediet <redietkfle@gmail.com>
2023-04-10 12:08:20 +0000

    Raise an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module creates empty class."""
+
+
+class BaseGeometry:
+    """Creates an empty class."""
+
+    def area(self):
+        """Class method not implemented."""
+
+        raise Exception("area() is not implemented")
