675529770bef6fb3f46003b336aabcea2681dc5a
rediet <redietkfle@gmail.com>
2023-04-10 18:03:00 +0000

    Inverts equality & negation

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,11 +5,9 @@
 class MyInt(int):
     """Creates class that inherits from int."""
 
-
     def __eq__(self, other):
         """Runs negation check."""
         return int.__ne__(self, other)
-
 
     def __ne__(self, other):
         """Runs negation check."""
