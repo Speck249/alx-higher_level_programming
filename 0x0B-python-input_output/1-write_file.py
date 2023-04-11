0fbf8512d19548d8b3a1f5309f20ea1fecf232e8
rediet <redietkfle@gmail.com>
2023-04-11 09:40:52 +0000

    Write into file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3 
+"""Module presents function that writes into file."""
+
+
+def write_file(filename="", text=""):
+    """Function writes into file.
+
+    Args:
+       filename: to be written into.
+       text: to be written into file.
+
+    Returns:
+       length of text.
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
+
+    f.close()
