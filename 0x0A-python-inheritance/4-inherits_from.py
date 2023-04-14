6833e8f96d5bf233622c358748b972e821d59842
rediet <redietkfle@gmail.com>
2023-04-14 06:27:22 +0000

    Validate subclass inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Module presents function that checks instance."""
+"""Module checks for subclass inheritance."""
 
 
 def inherits_from(obj, a_class):
-    """Function checks if object is instance
-    of a subclass.
- 
+    """Function validates subclass inheritance.
+
     Args:
-        obj: instance
-        a_class: subclass
-
-    Returns:
-        returns parent class if True.
-
+        obj: instance of a class
+        a_class: class
     """
 
     if type(obj) is a_class:
         return False
- 
+
     return isinstance(obj, a_class)
