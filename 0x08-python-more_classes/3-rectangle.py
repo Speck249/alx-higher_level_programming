b7f09614642ae873cf72a1a399c3f296a2502030
rediet <redietkfle@gmail.com>
2023-04-15 18:09:15 +0000

    Return rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Method instantiates new class.
+
         Args:
             width: first parameter
             height: second paramter
@@ -18,6 +19,7 @@ class Rectangle:
     @property
     def width(self):
         """Method retrieves width.
+
         Returns:
             value of width.
         """
@@ -27,8 +29,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Method sets value of width.
+
         Args:
             value: parameter
+
         Raises:
             TypeError: if width is not int.
             ValueError: if width < 0.
@@ -49,8 +53,10 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Method sets height value.
+
         Args:
             value: parameter
+
         Raises:
             TypeError: if height is not int.
             ValueError: if height < 0.
@@ -74,7 +80,11 @@ class Rectangle:
         Returns: perimeter of rectangle.
         """
 
-        return (2 * self.width) + (2 * self.height)
+        if self.width == 0 and self.height == 0:
+            return 0
+
+        else:
+            return (2 * self.width) + (2 * self.height)
 
     def __str__(self):
         """Method prints rectangle."""
