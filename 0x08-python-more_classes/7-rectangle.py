02bc9e1073876116431a56f97b3e060bcccf90f2
rediet <redietkfle@gmail.com>
2023-04-15 18:38:35 +0000

    Return string representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -98,7 +98,7 @@ class Rectangle:
             return empty_string
 
         else:
-            return ('\n'.join(str(self.print_symbol) * self.width
+            return ('\n'.join(str(self.print_symbol) * self.width \
             for _ in range(self.height)))
 
     def __repr__(self):
