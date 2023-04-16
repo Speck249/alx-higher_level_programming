81fab1988561de58bfac693c1592bae5e3955be3
rediet <redietkfle@gmail.com>
2023-04-16 04:11:04 +0000

    Insert new line

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module presents function for new line insetion."""
+
+def append_after(filename="", search_string="", new_string=""):
+    """Method inserts new line."""
+    my_str = ""
+
+    with open(filename, encoding="utf-8") as f:
+        for text in f:
+            my_str = my_str + text
+            if search_string in text:
+                my_str = my_str + new_string
+
+    with open(filename, 'w', encoding="utf-8") as g:
+        g.write(my_str)
