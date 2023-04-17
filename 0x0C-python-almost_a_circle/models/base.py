e57cc21aadc8646ff266a8097e4170702af45ffc
rediet <redietkfle@gmail.com>
2023-04-17 18:17:18 +0000

    Models for testing

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -78,3 +78,23 @@ class Base:
                     new_list1.append(cls.create(**ls))
 
         return new_list1
+
+    def save_to_file_csv(cls, list_objs):
+        """Method serializes CSV file."""
+
+        filename = "{}.csv".format(cls.__name__)
+
+        with open(filename, 'w', lines="") as f:
+            if list_objs is None or list_objs == []:
+                f.write('[]')
+            else:
+                if cls.__name__ == "Square":
+                    column = ['id', 'size', 'x', 'y']
+
+                elif cls.__name__ == "Rectangle":
+                    column = ['id', 'width', 'height', 'x', 'y']
+
+                csvfile = csv.DictWriter(f, column=column)
+
+                for items in list_objs:
+                    csvfile.writerow(ls.to_dictionary())
