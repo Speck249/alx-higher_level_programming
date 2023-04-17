c8aae221d52a29cf03553c9d9b73b328a7da3fb6
rediet <redietkfle@gmail.com>
2023-04-17 06:05:25 +0000

    Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -92,8 +92,8 @@ class Rectangle:
             return empty_string
 
         else:
-            return ('\n'.join(str(self.print_symbol) * self.width
-                for _ in range(self.height)))
+            return '\n'.join(str(self.print_symbol) *
+                             self.width for _ in range(self.height))
 
     def __repr__(self):
         """Method returns string representation of rectangle."""
