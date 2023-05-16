88fe8948b8a46fed64c4d6f29234ff9e6c42031d
Rediet <redietkfle@gmail.com>
2023-05-16 11:59:13 -0700

    Convert to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Convert database, table and field to UTF8
+-- (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
