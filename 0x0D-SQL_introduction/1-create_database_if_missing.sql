339b2a6b32e533cfda4a3b9866417fbc0f7bac09
Rediet <redietkfle@gmail.com>
2023-05-17 11:49:38 -0700

    Create a new database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- Command creates hbtn_0c_0 database
--- Command will not fail if database exists
+-- SQL Query creates hbtn_0c_0 database
+-- Query will not fail if database exists
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
