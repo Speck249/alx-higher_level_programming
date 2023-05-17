ee16703ab013097eef7e1377fdc581a9052fd73c
Rediet <redietkfle@gmail.com>
2023-05-17 11:29:23 -0700

    Display common values

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- SQL Query displays common value of two tables via SELECT.
 -- Query lists values by id(states) and name(cities).
 -- Query uses FOREIGN KEY and PRIMARY KEY.
-SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name="California" ORDER BY id);
+SELECT id, name FROM cities WHERE state_id=(SELECT id FROM states WHERE name="California" ORDER BY id);
