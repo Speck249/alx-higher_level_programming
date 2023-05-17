deae057449f7934c5d2de822b794b25e4a74d418
Rediet <redietkfle@gmail.com>
2023-05-17 10:40:05 -0700

    Create new user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- SQL Query creates new user with all privileges.
+-- Query will not fail if user already exists.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
