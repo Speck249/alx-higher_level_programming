17cdcaf1e66fe42160c13a0b1f1a75f065f14dec
Rediet <redietkfle@gmail.com>
2023-05-17 11:30:05 -0700

    JOIN TABLES

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- SQL Query lists all shows with at least one genre linked.
+-- Query uses LEFT JOIN
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
