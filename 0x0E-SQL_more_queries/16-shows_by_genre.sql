d144c88ada9454da265f207d9b78e6737874662f
Rediet <redietkfle@gmail.com>
2023-05-19 05:17:32 -0700

    Display all shows with their corresponding genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- SQL Query lists all shows.
+-- Query then lists all genres linked to show.
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
