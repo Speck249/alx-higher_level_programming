25cf45dfde181aa5b52bd7fb1510490ceb477af6
Rediet <redietkfle@gmail.com>
2023-05-19 04:15:08 -0700

    List all Comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- SQL Query lists all Comedy shows.
+-- Query sorts results by show title.
+SELECT tv_shows.title FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title
