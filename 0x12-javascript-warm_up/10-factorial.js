21a6efc2978ab08675a55ff429daa77c79bca030
Rediet <redietkfle@gmail.com>
2023-06-12 05:09:14 -0700

    Return factorial of num

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (num === 1 || (!parseInt(num))) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
