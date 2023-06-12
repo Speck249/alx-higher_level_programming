e68b93e88b25732d02677e37459222715c4d72f3
Rediet <redietkfle@gmail.com>
2023-06-12 03:28:25 -0700

    Check for Command Line Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const len = process.argv.slice(2);
-if (len === 0) {
+const len = process.argv.length;
+if (len === 2) {
   console.log('No argument');
-} else if (len === 1) {
+} else if (len === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
