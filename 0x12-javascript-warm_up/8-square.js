469574a909e7c7793f47ee77d8cd6d9c4d1d7134
Rediet <redietkfle@gmail.com>
2023-06-12 22:29:32 -0700

    Print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,13 +5,11 @@ const len = process.argv.slice(2);
 if (len === 0 || (!parseInt(len[0]))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < len[0]; i++) {
-    let j = 0;
-    let res = '';
-    while (j < len[0]) {
-      res = res + 'X';
-      j++;
-    }
-    console.log(res);
-  }
+	let sq = "";
+	for (i = 0; i < len[0]; i++) {
+		sq = sq + 'X';
+        }
+        for (i = 0; i < len[0]; i++) {
+		console.log(sq)
+        }
 }
