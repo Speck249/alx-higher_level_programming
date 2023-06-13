2e14d7ea4d9c699da40ee40aef889abf4b7576c0
Rediet <redietkfle@gmail.com>
2023-06-13 05:04:00 -0700

    Add new instance methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor(w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w
-			this.height = h
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print() {
-		let rect = "";
-		for (let i = 0; i < this.width; i++) {
-			rect = rect + 'X';
-                }
-                for (let i = 0; i < this.height; i++) {
-			console.log(rect)
-		}
-	}
+  print () {
+    let rect = '';
+    for (let i = 0; i < this.width; i++) {
+      rect = rect + 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(rect);
+    }
+  }
 
-	rotate() {
-		const temp = this.width;
-		this.width = this.height;
-		this.height = temp;
-	}
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
 
-	double() {
-		this.weight *= 2;
-		this.height *= 2;
-	}
+  double () {
+    this.weight *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
