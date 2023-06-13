b90f6bb7d5028dc285150a470f842ae3d63dc1a4
Rediet <redietkfle@gmail.com>
2023-06-13 00:05:31 -0700

    Create new instance method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const mainSquare = require('./5-square.js');
+
+class Square extends mainSquare {
+  charprint (c) {
+    if (c === undefined) {
+      c = 'X';
+    } else {
+      let sq = '';
+      for (let i = 0; i < this.width; i++) {
+        sq = sq + 'c';
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(sq);
+      }
+    }
+  }
+}
+
+module.exports = Square;
