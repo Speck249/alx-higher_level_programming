94c602857b0ffed0900dc83db63aaeed44206ade
Rediet <redietkfle@gmail.com>
2023-06-17 01:16:10 -0700

    Filter safe from SQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module displays all values in states table
+where name matches search term via script
+that is safe from MySQL injections.
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    """
+    Connect to MySQL server and
+    retrieve desired output.
+    """
+    db_connection = MySQLdb.connect(user=sys.argv[1],
+        password=sys.argv[2], database=sys.argv[3],
+        host='localhost', port=3306)
+
+    state_searched = sys.argv[4]
+
+    cursor = db_connection.cursor()
+
+    cursor.execute("SELECT * FROM states \
+            WHERE name = %s \
+            ORDER BY states.id;",(state_searched,))
+
+    result = cursor.fetchall()
+
+    for result in cursor:
+        print(result);
+
+    cursor.close()
+    db_connection.close()
