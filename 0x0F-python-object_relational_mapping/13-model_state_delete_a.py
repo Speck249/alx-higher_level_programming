c30ef729fd5ea6fcf5731e77cc54406e34ecb72b
Rediet <redietkfle@gmail.com>
2023-06-17 13:15:21 -0700

    Delete objects

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module deletes State objects with the letter 'a'
+from database hbtn_0e_6_usa.
+"""
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__  == '__main__':
+    """
+    Connect to MySQL server and
+    retrieve the desired output.
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    delete_obj = session.query(State).filter(State.name.like('%a%')).all()
+
+    for obj in delete_obj:
+        session.delete(obj)
+
+    session.commit()
