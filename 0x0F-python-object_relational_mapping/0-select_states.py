ce7ca332a4c0c232712bc082c24b392ce6564a38
Rediet <redietkfle@gmail.com>
2023-06-16 22:05:22 -0700

    List all states from database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     cursor = db_connection.cursor()
     
     query = "SELECT * FROM states ORDER BY states.id;"
-    
+
     cursor.execute(query)
 
     result = cursor.fetchall()
-    
+ 
     for item in result:
         print(item)
