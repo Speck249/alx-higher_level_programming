0a2343f27a87ee9795a79142284d4469229ee28f
Rediet <redietkfle@gmail.com>
2023-06-17 02:16:34 -0700

    Create table with two columns

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module creates Base class and
+an inheriting State class to
+work with SQLAlchemy ORM.
+"""
 from sqlalchemy import MetaData
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,6 +11,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    """
+    Creates table that stores unique state id(column_1)
+    along with their names(column_2).
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key= True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
