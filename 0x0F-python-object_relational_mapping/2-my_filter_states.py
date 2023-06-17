73b22d75f5ef032a592b9c7de4e94a16168d83d4
Rediet <redietkfle@gmail.com>
2023-06-17 00:58:33 -0700

    Display states that match search term

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module displays all values in states
+where name matches argument.
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    """
+    Connect to MySQL server and
+    display desired output.
+    """
+    db_connection = MySQLdb.connect(user=sys.argv[1],
+        password=sys.argv[2], database=sys.argv[3],
+        host='localhost', port=3306)
+
+    cursor = db_connection.cursor()
+    
+    state_searched = sys.argv[4]
+    
+    query = "SELECT * FROM states \
+            WHERE name LIKE BINARY '{}' \
+            ORDER BY states.id;".format(state_searched)
+    
+    cursor.execute(query)
+    
+    result = cursor.fetchall()
+
+    for item in result:
+        print(item);
+
+    cursor.close()
+    db_connection.close()
