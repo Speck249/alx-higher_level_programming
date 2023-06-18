ffc22f0ce38baf77470d7b9934cd0da1a9570f6c
Rediet <redietkfle@gmail.com>
2023-06-18 05:25:47 -0700

    List all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,23 @@
 Module lists all states
 from the database hbtn_0e_0_usa.
 """
+
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
     """
     Connect to the MySQL server
-    and retrieve desired output.
+    and retrieve output.
     """
     db_connection = MySQLdb.connect(user=sys.argv[1],
-        password=sys.argv[2], database=sys.argv[3],
-        host='localhost', port=3306)
+                                    password=sys.argv[2],
+                                    database=sys.argv[3],
+                                    host='localhost', port=3306)
 
     cursor = db_connection.cursor()
 
-    query = "SELECT * FROM states;"
+    query = "SELECT * FROM states ORDER BY states.id;"
 
     cursor.execute(query)
 
@@ -27,4 +29,5 @@ if __name__ == '__main__':
         print(item)
 
     cursor.close()
+
     db_connection.close()
