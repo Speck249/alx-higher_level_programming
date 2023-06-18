a91c5eda6bbcc7b686a3b652d53b1ae07e41f8cb
Rediet <redietkfle@gmail.com>
2023-06-18 06:35:37 -0700

    Add new State object

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """
 Module adds new State object
-to the database hbtn_0e_6_usa.
+to database hbtn_0e_6_usa.
 """
+import sys
 from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-import sys
 
-if __name__  == '__main__':
+if __name__ == '__main__':
     """
     Connect to MySQL server and
-    retrieve the desired output.
+    retrieve desired output.
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:'
+                           '{}@localhost:3306/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
 
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
     new_obj = State(name='Louisiana')
+
     session.add(new_obj)
+
     session.commit()
-    
+
     print("{}".format(new_obj.id))
