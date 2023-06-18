4c701f3951d3dbbffc67bda79ca5f2176c0e606f
Rediet <redietkfle@gmail.com>
2023-06-18 05:56:15 -0700

    List cities linked to a state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Module displays all cities of a state
-passed as an argument
+Module displays all cities of
+state passed as an argument
 """
 import sys
 import MySQLdb
@@ -9,25 +9,26 @@ import MySQLdb
 if __name__ == '__main__':
     """
     Connect to MySQL server and
-    retrieve desired output.
+    retrieve output.
     """
     db_connection = MySQLdb.connect(user=sys.argv[1],
-        password=sys.argv[2], database=sys.argv[3],
-        host='localhost', port=3306)
+                                    password=sys.argv[2],
+                                    database=sys.argv[3],
+                                    host='localhost', port=3306)
 
     state_name = sys.argv[4]
 
     cursor = db_connection.cursor()
 
-    cursor.execute("SELECT cities.id, cities.name, states.name \
-            FROM cities \
-            JOIN states ON cities.state_id = states.id \
-            WHERE states.name = '{}' \
-            ORDER BY states.id;".format(state_name))
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+                   "FROM cities JOIN states ON cities.state_id = states.id"
+                   " WHERE states.name = '{}' ORDER BY states.id;"
+                   .format(state_name))
 
     result = cursor.fetchall()
 
     print(", ".join([item[1] for item in result]))
 
     cursor.close()
+
     db_connection.close()
