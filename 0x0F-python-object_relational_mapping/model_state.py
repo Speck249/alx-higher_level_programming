4539746a945a21ea5325edf576fc784a234d0e73
Rediet <redietkfle@gmail.com>
2023-06-18 06:12:40 -0700

    Create class definition of State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,17 +4,18 @@ Module creates Base class and
 an inheriting State class to
 work with SQLAlchemy ORM.
 """
-from sqlalchemy import MetaData
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
-    Creates table that stores unique state id(column_1)
-    along with their names(column_2).
+    Creates table that stores
+    unique state ids and names.
     """
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key= True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
