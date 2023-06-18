db514b82bae04294fb368f26967cae588e21887b
Rediet <redietkfle@gmail.com>
2023-06-18 07:57:03 -0700

    Create new State and City objects

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -18,8 +18,8 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True, 
+    id = Column(Integer, primary_key=True, unique=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
+                          cascade='all, delete')
