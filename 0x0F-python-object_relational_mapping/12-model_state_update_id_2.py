2ea064252555e9c4ec80c00d6ebb88f32dbb0e60
Rediet <redietkfle@gmail.com>
2023-06-18 06:38:30 -0700

    Update State object

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 """
-Module updates name of State object
-in the database hbtn_0e_6_usa.
+Module updates State object name
+in database hbtn_0e_6_usa.
 """
+import sys
 from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-import sys
 
-if __name__  == '__main__':
+if __name__ == '__main__':
     """
     Connect to MySQL server and
-    retrieve the desired output.
+    retrieve desired output.
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:'
+                           '{}@localhost:3306/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
 
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
     target = session.query(State).get(2)
+
     target.name = 'New Mexico'
+
     session.commit()
