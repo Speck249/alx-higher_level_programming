0c4f815fd5768c11d956a3b6b7e7be950ba4e4ee
Rediet <redietkfle@gmail.com>
2023-06-18 06:32:46 -0700

    List states objects matching search term

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 """
-Module lists the id of the first
-State object that matches search term
-from database hbtn_0e_6_usa.
+Module lists State object id
+that matches search term from
+database hbtn_0e_6_usa.
 """
-from sqlalchemy import create_engine, or_
+import sys
+from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-import argparse
-import sys
 
-if __name__  == '__main__':
+if __name__ == '__main__':
     """
     Connect to MySQL server and
-    retrieve the desired output.
+    retrieve desired output.
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:'
+                           '{}@localhost:3306/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
 
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    result = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    result = session.query(State).filter(State.name == sys.argv[4])\
+        .first()
 
     if result is None:
         print('Not found')
