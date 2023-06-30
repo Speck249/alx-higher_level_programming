bb060cfce897fb49bdfae5f2a7e7456eecd64cea
Rediet <redietkfle@gmail.com>
2023-06-30 00:24:29 -0700

    Display value of X-Request-Id via header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module displays value of
+X-Request-Id variable found
+in header of response.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = requests.get(url)
+    
+    output = res.headers.get("X-Request-Id")
+    print(output)
