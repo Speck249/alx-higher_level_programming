638f1f9e1aeb914b18ec0356c4d9933e7800f590
Rediet <redietkfle@gmail.com>
2023-06-30 00:25:23 -0700

    Manage urllib.error.HTTPError exceptions via requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module urllib.error.HTTPError
+exceptions.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
+
