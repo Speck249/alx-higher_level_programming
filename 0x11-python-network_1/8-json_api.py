c63dddc73fcc1f5b49a52fbbd783bc151cfa1f33
Rediet <redietkfle@gmail.com>
2023-06-30 06:25:06 -0700

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,9 +8,9 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        q=sys.argv[1]
+        q = sys.argv[1]
     elif len(sys.argv) == 1:
-        q=""
+        q = ""
 
     url = "http://0.0.0.0:5000/search_user"
     param = {'q': q}
