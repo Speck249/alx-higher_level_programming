12b435dae195b8a1c69ec8cfc59b18a7b51cf5ee
Rediet <redietkfle@gmail.com>
2023-07-25 13:00:04 +0000

    Print episode title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId + '/';
 request(url, function (error, response, body) {
   if (error == null) {
-     const result = JSON.parse(body);
-     console.log(result.title);
-     }
+    const result = JSON.parse(body);
+    console.log(result.title);
+  }
 });
