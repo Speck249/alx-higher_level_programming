c9cc7ed534f5d53fb1c1488f5b0bb125df926c9f
Rediet <redietkfle@gmail.com>
2023-07-25 12:04:22 +0000

    Read and print file content

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file_path = process.argv[2];
-fs.readFile(file_path, 'utf-8', (err, data) => {
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
     return;
