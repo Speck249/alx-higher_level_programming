4ff6f15f0fbc07e57acfb821e4252cdf80e463bf
Rediet <redietkfle@gmail.com>
2023-07-25 12:15:27 +0000

    Write content to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file_path = process.argv[2];
-const string = process.argv[3]
-fs.writeFile(file_path, string, 'utf-8', (err, data) => {
-   if (err) {
-      console.error(err);
-      return;
-   }
+const file = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(file, string, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 });
