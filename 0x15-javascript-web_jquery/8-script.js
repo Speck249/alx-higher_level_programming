5dadc383c580f0ceae62e91c8be6c4f205ef7827
Rediet <redietkfle@gmail.com>
2023-08-02 10:15:06 +0000

    Fetch movie titles

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,8 +1,8 @@
-$.getJSON("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
   const movies = data.results;
   const list = $('#list_movies');
-  $.each(movies, function(index, movie) {
-    const item = $("<li>").text(movie.title);
+  $.each(movies, function (index, movie) {
+    const item = $('<li>').text(movie.title);
     list.append(item);
   });
 });
