0a66f7c73b18c5f4df41825f394bcd031e916482
Rediet <redietkfle@gmail.com>
2024-02-09 18:30:37 -0800

    Print all names from imported compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+import hidden_4
 
-    import hidden_4
-
+def main():
     for name in dir(hidden_4):
         if name[:2] != "__":
             print(name)
+
+if __name__ == "__main__":
+    main()
