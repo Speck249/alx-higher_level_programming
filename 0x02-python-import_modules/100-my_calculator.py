6ac4163e88a7c33df3f0d36eeca2ee0d8699b8c1
Rediet <redietkfle@gmail.com>
2024-02-09 18:54:46 -0800

    Create calculator program for basic arithmetic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-
-    length = len(sys.argv)
-    result = 0
-
-    if (length - 1)  != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+def main():
+    if len(sys.argv) <= 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
+    else:
+        x = int(sys.argv[1])
+        y = sys.argv[2]
+        z = int(sys.argv[3])
 
-    operator = sys.argv[2]
+        if y == '+':
+            print('{} {} {} = {}'.format(x, y, z, add(x, z)))
+        elif y == '-':
+            print('{} {} {} = {}'.format(x, y, z, sub(x, z)))
+        elif y == '*':
+            print('{} {} {} = {}'.format(x, y, z, mul(x, z)))
+        elif y == '/':
+            print('{} {} {} = {}'.format(x, y, z, div(x, z)))
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            sys.exit(1)
 
-    if operator != '+' and operator != '-' and operator != '*' and operator != '/':
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-
-    if operator == '+':
-        result = add(a, b)
-        print("{:d} + {:d} = {:d}".format(a, b, result))
-
-    elif operator == '-':
-        result = sub(a, b)
-        print("{:d} - {:d} = {:d}".format(a, b, result))
-
-    elif operator == '*':
-        result = div(a, b)
-        print("{:d} * {:d} = {:d}".format(a, b, result))
-
-    elif operator == '/':
-        result = div(a, b)
-        print("{:d} / {:d} = {:d}".format(a, b, result))
+if __name__ == '__main__':
+    main()
