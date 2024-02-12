2456479b639d9969c6968db12afd487c11960421
Rediet <redietkfle@gmail.com>
2024-02-12 05:56:12 -0800

    Return string with Omitted letter

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = ""
-    for c in my_string:
-        if c != "C" and c != "c":
-            new_string += c
+    new_string = ''
+    for idx in range(len(my_string)):
+        if my_string[idx] != 'C' and my_string[idx] != 'c':
+            new_string += my_string[idx]
     return new_string
