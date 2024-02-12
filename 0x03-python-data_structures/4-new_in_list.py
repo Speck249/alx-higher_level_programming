e4032aa9c7e9dd610d12fbb68c43e19be1e56bf8
Rediet <redietkfle@gmail.com>
2024-02-12 05:43:44 -0800

    Modify copy of list at a given index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-
+"""
+Shallow copies are affected by modifications
+in the event of nesting
+"""
 def new_in_list(my_list, idx, element):
-    if my_list:
-        if idx < 0:
-            return my_list
-
-        n = len(my_list)
-
-        if idx >= n:
-            return my_list
-        else:
-            new_list = my_list.copy()
-            new_list[idx] = element
-            return new_list
+    new_list = my_list[:]
+    if idx < 0 or idx > len(my_list) - 1:
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
