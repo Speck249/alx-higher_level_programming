6806469ab036e0b05e6084b8c2137456663ee881
Rediet <redietkfle@gmail.com>
2024-02-13 05:19:41 -0800

    Determine if list is divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/usr/python3
 
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for x in my_list:
-        if x % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
+    new_list = [num % 2 == 0 for num in my_list]
     return new_list
