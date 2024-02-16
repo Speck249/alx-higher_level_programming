b1fb87afc66ba30c6f85530deb60bbb4cbdd0d03
Rediet <redietkfle@gmail.com>
2024-02-16 03:44:33 -0800

    Return list values multiplied by an integer

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return 0
-
-    num = 0
-    denum = 0
-    for i in my_list:
-        num += i[0] * i[1]
-        denum += i[1]
-
-    return round(num / denum, 2)
+    if my_list:
+        numerator = [a * b for a, b in my_list[0:]]
+        denominator = [num[1] for num in my_list]
+        weighted_avg = sum(numerator) / sum(denominator)
+        return weighted_avg
+    return 0
