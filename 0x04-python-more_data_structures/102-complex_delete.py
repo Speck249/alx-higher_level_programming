0931828f4a249ec7d8163d268d6580e91663b7c4
Rediet <redietkfle@gmail.com>
2024-02-16 04:23:00 -0800

    Delete all occurences of values of a given key

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,10 +3,10 @@
 def complex_delete(a_dictionary, value):
     new_dict = []
 
-    for i in a_dictionary:
-        if a_dictionary[i] == value:
-            new_dict.append(i)
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            new_dict.append(key)
 
-    for j in new_dict:
-        del a_dictionary[j]
+    for key in new_dict:
+        del a_dictionary[key]
     return a_dictionary
