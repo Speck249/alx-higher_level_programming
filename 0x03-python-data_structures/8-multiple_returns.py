3676bcddeabb6ab0f36e6da83d8534a5d561c756
Rediet <redietkfle@gmail.com>
2024-02-16 04:28:10 -0800

    Return string length and its first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (len(sentence), None)
-    elif len(sentence) > 0:
-        return (len(sentence), sentence[0])
+    length = len(sentence)
+
+    if length > 0:
+        return (length, sentence[0])
+    else:
+        return (length, None)
