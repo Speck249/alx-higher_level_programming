b24098f2a1a268c1a7fc5697d7aa7084411b2f1b
Rediet <redietkfle@gmail.com>
2024-02-16 04:11:43 -0800

    Return weighted average of a list of tuples

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 
 def weight_average(my_list=[]):
     if my_list:
-        numerator = [a * b for a, b in my_list[0:]]
+        numerator = [x * y for x, y in my_list[0:]]
         denominator = [num[1] for num in my_list]
         weighted_avg = sum(numerator) / sum(denominator)
         return weighted_avg
