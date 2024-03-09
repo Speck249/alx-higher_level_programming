c4e9cc1e797118ca02a8f378b280271ba3bc7247
rediet <redietkfle@gmail.com>
2024-03-09 03:27:58 -0800

    Define instance methods that execute operator overloading

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,35 +1,46 @@
 #!/usr/bin/python3
-"""Python Classes & Objects."""
+"""
+Module executes operator overloading.
+"""
 
 
 class Square:
-    """Creates a class."""
+    """
+    Creates new class.
+    """
 
     def __init__(self, size=0):
-        """Instantiates the new class.
+        """
+        Constructor method initializes new class
+        with private attribute.
+
         Args:
             size: size of the square.
+
         Raises:
-            TypeError: if size is not an integer.
-            ValueError: if value of size is negative.
+            TypeError: size must be an integer.
+            ValueError: size must be positive integer.
         """
 
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-
         self.__size = size
 
     @property
     def size(self):
-        """Retrieves size of square"""
+        """
+        Getter method retrieves size of square.
+        """
 
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets size of square."""
+        """
+        Setter method modifies value of square size.
+        """
 
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
@@ -39,27 +50,106 @@ class Square:
 
     def area(self):
         """
-        Calculate area of the square from given size.
-        Returns: area of the square.
+        Instance method calculates area of square.
+
+        Returns:
+          area of the square.
         """
 
-        result = self.__size ** 2
-        return result
+        return self.__size ** 2
 
     def __eq__(self, other):
+        """
+        Instance method evaluates equality of instance
+        values.
+
+        Args:
+          self: accesses instance attribute value.
+          other: signifies to operator that instance
+                 represents second operand.
+
+        Returns:
+          True if instance values are equal, False otherwise.
+        """
+
         return self.__size == other.__size
 
     def __ne__(self, other):
+        """
+        Instance method evaluates equality of instance
+        values.
+
+        Args:
+          self: accesses instance attribute value.
+          other: signifies to operator that instance
+                 represents second operand.
+
+        Returns:
+          True if instance values are not equal, False otherwise.
+        """
+
         return self.__size != other.__size
 
     def __gt__(self, other):
+        """
+        Instance method evaluates instance values.
+
+        Args:
+          self: accesses instance attribute value.
+          other: signifies to operator that instance
+                 represents second operand.
+
+        Returns:
+          True if first instance value is greater than
+          second instance value, False otherwise.
+        """
+
         return self.__size > other.__size
 
     def __ge__(self, other):
+        """
+        Instance method evaluates instance values.
+
+        Args:
+          self: accesses instance attribute value.
+          other: signifies to operator that instance
+                 represents second operand.
+
+        Returns:
+          True if first instance value is greater than
+          or equal to second instance value, False otherwise.
+        """
+
         return self.__size >= other.__size
 
     def __lt__(self, other):
+        """
+        Instance method evaluates instance values.
+
+        Args:
+          self: accesses instance attribute value.
+          other: signifies to operator that instance
+                 represents second operand.
+
+        Returns:
+          True if first instance value is less than
+          second instance value, False otherwise.
+        """
+
         return self.__size < other.__size
 
     def __le__(self, other):
+        """
+        Instance method evaluates instance values.
+
+        Args:
+          self: accesses instance attribute value.
+          other: signifies to operator that instance
+                 represents second operand.
+
+        Returns:
+          True if first instance value is less than or
+          equal to second instance value, False otherwise.
+        """
+
         return self.__size <= other.__size
