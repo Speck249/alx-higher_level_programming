be5a8d47ad2193f2de46d8249d75d64c10c7250e
rediet <redietkfle@gmail.com>
2024-03-09 02:18:56 -0800

    Create an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Module defines an empty class.
+Module defines an empty class
 """
 
 
 class Square:
     """
-    New and empty class created.
+    Creates a new empty class
     """
     pass
