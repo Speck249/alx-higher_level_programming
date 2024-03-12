c96d9f50de9c949a8406866ed54ed6a96dd52059
rediet <redietkfle@gmail.com>
2024-03-12 06:39:18 -0700

    Prevent dynamic creation of instance attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""Module prevents instance instantiation."""
+"""
+Module prevents dynamic instance
+attribute creation.
+"""
 
 
 class LockedClass:
-    """Creates empty class."""
+    """
+    Explicity defines a fixed set of instance
+    attributes at class level. Any attribute not
+    listed in slots will raise an AttributeError. 
+    """
 
     __slots__ = ['first_name']
