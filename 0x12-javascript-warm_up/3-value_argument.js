98998f143ca0bc75d90c6ae728f69d0b703d06a0
rediet <redietkfle@gmail.com>
2024-03-20 06:02:46 -0700

    Print first command line argument, if any

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args[0] === undefined) {
+
+if (!(process.argv[2])) {
   console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(process.argv[2]);
 }
