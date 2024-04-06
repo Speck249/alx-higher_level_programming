6fd906c5825ade30e93b450565a532dcedbc2276
rediet <redietkfle@gmail.com>
2024-04-06 07:56:36 -0700

    Divide matrix elements by numerical value

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
-"""Module presents function that divides a matrix."""
+"""
+Module implements function that divides
+each matrix element by divider value.
+"""
+
 
 def matrix_divided(matrix, div):
-    """Divides matrix by an integer.
-    
-    Args:
-        matrix: list of lists to be divided by an int
-        div: the dividing integer
+    """
+    Function divides matrix by given number
+    and returns output.
 
-    Returns:
-        Division of matrix by div.
+    Args:
+        matrix: list of lists of integers/floats.
+        div: divider of integer/float type.
 
     Raises:
-        TypeError: if matrix is not integer or float.
-        TypeError: if matrix has unequal number of rows.
-        TypeError: if div is a string or equal to zero.
+        TypeError: matrix must be list of lists of integer/float type.
+        TypeError: length of nested lists must be equal.
+        TypeError: div must be integer or float type not equal to zero.
 
+    Returns:
+        New matrix with new values after division.
     """
 
-if not isinstance(matrix, list):
-if 
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    for element in matrix:
+        if not isinstance(element, list):
+            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+
+    length = len(matrix[0])
+    idx = 1
+    while idx < len(matrix):
+        if len(matrix[idx]) != length:
+            raise('Each row of the matrix must have the same size')
+        idx += 1
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    new_matrix = matrix[:]
+    for idx in range(len(new_matrix)):
+        new_matrix[idx] = [round((num / div), 2) for num in new_matrix[idx]]
+    return new_matrix
