70ffd98c3a3b710a6a366bcd23a9f20a04780315
rediet <redietkfle@gmail.com>
2024-04-09 07:40:08 -0700

    Deserialize JSON string from file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""Module presents function that creates object."""
+"""
+Module implements function that deserializes
+JSON string from a file.
+"""
+
 import json
 
 
 def load_from_json_file(filename):
-    """Function creates object from JSON file.
+    """
+    Function accepts file containing JSON
+    string, deconstructs and returns the
+    original Python object.
 
     Args:
-        filename: JSON file
+        filename: JSON file.
+
+    Returns:
+        Python object.
     """
 
-    with open(filename, 'r') as f:
-        return json.load(f)
-
-    f.close()
+    with open(filename, 'r', encoding='UTF-8') as file:
+        return json.load(file)
