84e7e0436fb7651bec452ac137944f23e185779c
rediet <redietkfle@gmail.com>
2024-04-10 06:46:26 -0700

    Append new line to file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Module presents function for new line insetion."""
+"""
+Module implements function that
+appends new line to file.
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Method inserts new line."""
-    my_str = ""
+    """
+    Function searches through file to find
+    line containing search word then appends
+    new line immediately after.
+    """
 
-    with open(filename, encoding="utf-8") as f:
-        for text in f:
-            my_str = my_str + text
-            if search_string in text:
-                my_str = my_str + new_string
+    with open(filename, 'r', encoding='UTF-8') as file:
+        lines = file.readlines()
 
-    with open(filename, 'w', encoding="utf-8") as g:
-        g.write(my_str)
+    with open(filename, 'w', encoding='UTF-8') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
