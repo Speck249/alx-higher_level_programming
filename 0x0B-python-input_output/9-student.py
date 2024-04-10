9c2f988fb9ab5187c299c88dc7a9f22009fdacd6
rediet <redietkfle@gmail.com>
2024-04-10 06:18:10 -0700

    Create instance method that retrieves dictionary representation

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-"""Module returns dictionary representation."""
+"""
+Module creates class with method
+that retrieves dictionary.
+"""
 
 
 class Student:
-    """Creates empty class."""
+    """
+    Creates new class.
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Instantiates class."""
+        """
+        Constructor method initializes class
+        with public attributes.
+
+        Args:
+            first_name
+            last_name
+            age
+        """
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Returns dictionary representation."""
+        """
+        Returns dictionary representation
+        of instance attributes.
+        """
 
         return (self.__dict__)
