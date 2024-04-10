83f72ec3ea9aeb95e0940cd082274beba69abd6b
rediet <redietkfle@gmail.com>
2024-04-10 04:19:52 -0700

    Convert Objects to JSON strings and save to file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""Module presents functions that saves to file."""
-import os.path
+"""
+Module converts Python Objects to JSON Strings.
+Output file with Object or String content is passed
+to 'load' function to ensure Object type. Output is
+appended to a new list. Otherwise, user passed args
+are appended. The list Object is passed to the 'save'
+function, converted to JSON format and saved to file.
+"""
+
 import sys
+import os.path
+
 
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 new_list = []
 
-if os.path.exists("add_item.json"):
-    new_list = load("add_item.json")
+if os.path.exists('add_item.json') and os.path.getsize('add_item.json') > 0:
+    new_list = load('add_item.json')
 
 for arg in sys.argv[1:]:
     new_list.append(arg)
 
-save(new_list, "add_item.json")
+save(new_list, 'add_item.json')
