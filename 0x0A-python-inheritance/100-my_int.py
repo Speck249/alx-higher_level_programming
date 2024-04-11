8fee9bbc6dfca3a007efa8427f9c3f64ffc54c8e
rediet <redietkfle@gmail.com>
2024-04-11 07:52:01 -0700

    Invert int class instance methods

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python3
-"""Module creates class that inherits from int."""
+"""
+Module inverts normal operation
+of integer class methods.
+"""
 
 
 class MyInt(int):
-    """Creates class that inherits from int."""
+    """
+    Creates new subclass.
+    """
 
     def __eq__(self, other):
-        """Runs negation check."""
+        """
+        Instance method checks for inequality.
+
+        Args:
+            self: accesses instance attribute value.
+            other: signifies to operator instance
+                   represents second operand.
+
+        Returns:
+            True if integer operands are not equal.
+        """
+
         return int.__ne__(self, other)
 
     def __ne__(self, other):
-        """Runs negation check."""
+        """
+        Instance method checks for equality.
+
+        Args:
+            self: accesses instance attribute value.
+            other: signifies to operator instance
+                   represents second operand.
+
+        Returns:
+            True if integer operands are equal.
+        """
+
         return int.__eq__(self, other)
