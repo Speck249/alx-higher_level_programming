72efb492b670cc16b18d08e315ddcd296ab3e85a
rediet <redietkfle@gmail.com>
2024-04-11 05:25:47 -0700

    Create subclass Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-"""Module inherits from Base Geometry."""
+"""
+Module creates subclass Rectangle.
+"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class inherits from Base Geometry."""
-
+    """
+    Creates new subclass that inherits
+    from BaseGeometry.
+    """
     def __init__(self, width, height):
-        """Method instantiates class."""
+        """
+        Constructor method initializes class
+        with private attributes.
+
+        Args:
+            width: width of rectangle.
+            height: height of rectangle.
+
+        Raises:
+            TypeError: if width is not a positive int.
+            TypeError: if height is not a positive int.
+        """
 
         self.__width = width
-        self.integer_validator("width", width)
-        """validates value of width."""
+        self.integer_validator('width', width)
 
         self.__height = height
-        self.integer_validator("height", height)
-        """validates value of height."""
+        self.integer_validator('height', height)
