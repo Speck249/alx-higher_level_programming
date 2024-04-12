b4bb3d8f46738017be2786d3efff0e96e651f641
rediet <redietkfle@gmail.com>
2024-04-12 07:48:13 -0700

    Return product of two matrices

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,19 +1,55 @@
 #!/usr/bin/python3
-"""Module presents function that multiplies two matrices."""
+"""
+Module implements function that
+returns product of two matrices.
+"""
 
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies two matrices using numpy.
+    """
+    Function accepts two matrices, calculates and
+    returns product with dot notation multiplication.
 
     Args:
-        m_a: list of lists of integers or floats.
-        m_b: list of lists of integers or floats. 
+        m_a: first matrix
+        m_b: second matrix
+
+    Raises:
+        TypeError: matrices must be list of lists.
+                   matrices must contain int or float elements.
+                   matrices must have compatible dimensions.
 
     Returns:
         Product of two matrices.
-
     """
 
-    m_c = np.dot(m_a, m_b)
-    return m_c
+    if type(m_a) is not list and type(m_b) is not list:
+        raise TypeError('matrix must be of type list')
+
+    for val in m_a:
+        if type(val) is not list:
+            raise TypeError('matrix must be list of lists')
+
+    for val in m_b:
+        if type(val) is not list:
+            raise TypeError('matrix must be list of lists')
+
+    for val in m_a:
+        for num in val:
+            if type(num) is not int and type(num) is not float:
+                raise TypeError('matrix lists must contain int or float.')
+
+    for val in m_b:
+        for num in val:
+            if type(num) is not int and type(num) is not float:
+                raise TypeError('matrix lists must contain int or float.')
+
+    m_a_dimension = np.array(m_a)
+    m_b_dimension = np.array(m_b)
+
+    if m_a_dimension.shape[1] != m_b_dimension.shape[0]:
+        raise TypeError('matrices must have compatible dimensions')
+
+    return np.dot(m_a, m_b)
