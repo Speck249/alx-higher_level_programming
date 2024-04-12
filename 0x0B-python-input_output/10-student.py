9874bd34dd5f24d803d0f635a41a409e4ec59a38
rediet <redietkfle@gmail.com>
2024-04-12 03:17:00 -0700

    Instance method returns class attributes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,35 +1,39 @@
 #!/usr/bin/python3
-"""Module returns dictionary representation."""
+"""
+Module returns class attributes.
+"""
 
 
 class Student:
-    """Creates empty class."""
-
+    """
+    Creates new class.
+    """
     def __init__(self, first_name, last_name, age):
-        """Instantiates class."""
-
+        """
+        Constructor method initializes class
+        with public attributes.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns dictionary representation."""
+        """
+        Instance method accepts list of attributes. After
+        type validation, if attributes belong to class,
+        method returns attribute-value pair in dictionary
+        format. Otherwise, instance attribute dictionary
+        is returned.
+        """
 
-        all_attr = self.__dict__.copy()
+        all_attributes = self.__dict__.copy()
 
-        if type(attrs) is list:
-            for a in attrs:
-                if type(a) is not str:
-                    return all_attr
+        if type(attrs) is not list:
+            return all_attributes
 
-            attr_dict = {}
+        for attr in attrs:
+            if type(attr) is not str:
+                return all_attributes
 
-            for i in range(len(attrs)):
-                for j in all_attr:
-                    if attrs[i] == j:
-                        attr_dict[j] = all_attr[j]
-
-            return attr_dict
-
-        else:
-            return all_attr
+        return {attr: all_attributes[attr] for attr
+                in attrs if attr in all_attributes}
