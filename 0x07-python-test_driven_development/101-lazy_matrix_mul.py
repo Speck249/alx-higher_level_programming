bf2d6ad9073743a5599aa43975011de184d2680a
rediet <redietkfle@gmail.com>
2024-04-13 12:24:25 -0700

    Raise numpy.matmul exceptions

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -10,55 +10,21 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """
     Function accepts two matrices, calculates and
-    returns product with dot notation multiplication.
+    returns matrix product using numpy.matmul.
 
     Args:
-        m_a: first matrix operand.
-        m_b: second matrix operand.
+        m_a: first input array operand.
+        m_b: second input array operand.
 
     Raises:
-        TypeError: matrix must be list of lists.
-                   matrices must contain int or float values.
-                   matrices must have compatible dimensions.
-        ValueError: neither matrices must be empty.
+        TypeError: invalid data type for einsum.
+        ValuError: scalar values must not be passed.
+                   input matrices must have compatible
+                   dimensions
 
     Returns:
-        Product of two matrices.
+        matrix product of inputs that's only scalar
+        if input arrays are 1-d vectors..
     """
 
-    if type(m_a) is not list and type(m_b) is not list:
-        raise TypeError('matrix must be a list')
-
-    for val in m_a:
-        if len(val) == 0:
-            raise ValueError('matrix must not be empty')
-
-    for val in m_b:
-        if len(val) == 0:
-            raise ValueError('matrix must not be empty')
-
-    for val in m_a:
-        if type(val) is not list:
-            raise TypeError('matrix must be list of lists')
-
-    for val in m_b:
-        if type(val) is not list:
-            raise TypeError('matrix must be list of lists')
-
-    m_a_dimension = np.array(m_a)
-    m_b_dimension = np.array(m_b)
-
-    if m_a_dimension.shape[1] != m_b_dimension.shape[0]:
-        raise TypeError('matrices must have compatible dimensions')
-
-    for val in m_a:
-        for num in val:
-            if type(num) is not int and type(num) is not float:
-                raise TypeError('matrix lists must contain int or float')
-
-    for val in m_b:
-        for num in val:
-            if type(num) is not int and type(num) is not float:
-                raise TypeError('matrix lists must contain int or float')
-
-    return (np.dot(m_a, m_b)).tolist()
+    return np.matmul(m_a, m_b)
