bf1a3fcf4821ef2147b62f48f6079d113b4bc291
rediet <redietkfle@gmail.com>
2024-04-24 06:10:54 -0700

    List all records of table from database

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
--- SQL Query lists names and scores
--- where records are ordered by score
+-- Query lists all names and scores from second_table
+-- Query orders results by score (descending)
 SELECT score, name FROM second_table ORDER BY score DESC;
