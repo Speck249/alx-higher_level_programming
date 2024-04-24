0a96cd6bc2a1b377ebc14a8b82a4e853ed665bca
rediet <redietkfle@gmail.com>
2024-04-24 06:15:25 -0700

    Display cities with the highest average temperatur

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,2 @@
--- Command calculates and displays
--- the average of temperatures.
+-- Query displays top three cities for July, and August ordered by temperature
 SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month=7 or month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
