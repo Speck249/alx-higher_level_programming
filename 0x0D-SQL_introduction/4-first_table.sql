7fdc93c611e4b5d79b97368935f4b2141956b984
rediet <redietkfle@gmail.com>
2024-04-24 06:07:56 -0700

    Create new table with fields

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
--- SQL Query creates new table.
--- Query will not fail if table already exists.
+-- Query creates new table inside hbtn_0c_0
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256));
