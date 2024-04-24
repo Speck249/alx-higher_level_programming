7f90d3e7b8a3f28e3531c179154fb620b1bfb3a9
rediet <redietkfle@gmail.com>
2024-04-24 06:11:23 -0700

    List all records with scores >= 10

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
--- Command lists names and scores
--- where records are ordered by score
+-- Query lists names and scores from second_table
+-- Query orders result by score >= 10
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
