a4d9975b9491fd91262240cd47a9efc016cf77c8
rediet <redietkfle@gmail.com>
2024-04-24 06:09:15 -0700

    Insert new rows into table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- SQL Query inserts new row in table
--- New row id and name values
+-- Query inserts new row in first_table inside hbtn_0c_0
+-- New row contains unique id and name value
 INSERT INTO first_table(id, name) VALUES (89, 'Best School');
