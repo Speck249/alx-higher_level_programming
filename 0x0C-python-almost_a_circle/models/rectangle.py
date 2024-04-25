e4012436f64fef678f9b7c49458ad3ebb8f35907
rediet <redietkfle@gmail.com>
2024-04-25 06:34:42 -0700

    Updare class Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,27 +1,46 @@
 #!/usr/bin/python3
-"""Method presents Rectangle methods."""
+"""
+Module defines class inheriting from Base class.
+"""
+
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Creates inherited class."""
+    """
+    Creates new subclass.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Class instantiates inherited class."""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        """
+        Constructor call initializes class with private instance
+        attributes and public superclass attribute.
+        """
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
     def width(self):
-        """Method retrieves width value"""
+        """ Getter method retrieves value of width. """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Method sets value of width."""
+        """
+        Setter method sets new width value.
+
+        Args:
+            value: new value of width
+
+        Raises:
+            TypeError: width must be integer.
+            ValueError: width must be positive non-zero.
+        """
+
         if type(value) is not int:
             raise TypeError('width must be an integer')
         if value <= 0:
@@ -30,12 +49,22 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Method retrieves height value"""
+        """ Getter method retrieves value of height. """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Method sets value of height."""
+        """
+        Setter method sets new height value.
+
+        Args:
+            value: new value of height
+
+        Raises:
+            TypeError: height must be integer.
+            ValueError: height must be positive non-zero.
+        """
+
         if type(value) is not int:
             raise TypeError('height must be an integer')
         if value <= 0:
@@ -44,12 +73,22 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """Method retrieves x's value"""
+        """ Getter method retrieves value of x. """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Method sets value of x."""
+        """
+        Setter method sets new x value.
+
+        Args:
+            value: new value of x-coordinate.
+
+        Raises:
+            TypeError: x-coordinate must be integer.
+            ValueError: x-coordinate must be positive.
+        """
+
         if type(value) is not int:
             raise TypeError('x must be an integer')
         if value < 0:
@@ -58,52 +97,85 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """Method retrieves width value"""
+        """ Getter method retrieves y of width. """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Method sets value of y."""
+        """
+        Setter method sets new y value.
+
+        Args:
+            value: new value of y-coordinate.
+
+        Raises:
+            TypeError: y-coordinate must be integer.
+            ValueError: y-coordinate must be positive.
+        """
+
         if type(value) is not int:
             raise TypeError('y must be an integer')
         if value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
 
+    def __str__(self):
+        """
+        Returns string representation of instance id,
+        x and y coordinates, rectangle width and height.
+        """
+        return f'[Rectangle] ({self.id}) {self.x}/{self.y} -' \
+               f' {self.width}/{self.height}'
+
     def area(self):
-        """Computes and returns area of rectangle."""
+        """
+        Instance method returns area of rectangle.
+
+        Args:
+            width: width of rectangle.
+            height: height of rectangle.
+
+        Returns:
+            Area of rectangle.
+        """
         return self.__width * self.__height
 
     def display(self):
-        """Prints rectangle to stdout."""
-        for new_line in range(self.y):
-            print("")
-        for rows in range(self.__height):
-            for indent in range(self.x):
-                print(" ", end="")
-            for columns in range(self.__width):
-                print("#", end="")
-            print()
+        """
+        Instance method prints rectangle to console
+        along x and y-coordinates.
+        """
 
-    def __str__(self):
-        """Method returns printed output."""
-        return "[Rectangle] ({:d}) {:d}/{:d} - " \
-               "{:d}/{:d}".format(self.id, self.x,
-                                  self.y, self.width, self.height)
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(' ' * self.x, end='')
+            print('#' * self.width)
 
     def update(self, *args, **kwargs):
-        """Method assigns arguments."""
-        length = len(args)
+        """
+        Instance method updates attribute values.
 
-        if args is not None and length != 0:
-            attributes = ['id', 'width', 'height', 'x', 'y']
-            for j in range(length):
-                setattr(self, attributes[j], args[j])
-        else:
-            for name, value in kwargs.items():
-                setattr(self, name, value)
+        Args:
+            *args: new instance attribute values.
+            **kwargs: dictionary of attribute name
+            and corresponding values.
+
+        Returns:
+            Updated attribute values.
+        """
+
+        attr = ('id', 'width', 'height', 'x', 'y')
+        for idx in range(len(args)):
+            setattr(self, attr[idx], args[idx])
+        if not args:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        """Method returns dictionary representation of Rectangle"""
-        return {'id': self.id, 'width': self.__width,
-                'height': self.__height, 'x': self.__x, 'y': self.__y}
+        """
+        Instance method returns dictionary containing
+        attribute-value pair of class Rectangle.
+        """
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width}
