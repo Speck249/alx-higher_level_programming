0e82da2b4838a0eef8a521b0f368daf6a6b3f912
rediet <redietkfle@gmail.com>
2024-04-29 11:56:22 -0700

    Return dictionary values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    copy_of_a_dictionary = a_dictionary.copy()
-    for key, value in copy_of_a_dictionary.items():
-        copy_of_a_dictionary[key] = value * 2
-    return copy_of_a_dictionary
+    return {key: val * 2 for key, val in sorted(a_dictionary.items())}
