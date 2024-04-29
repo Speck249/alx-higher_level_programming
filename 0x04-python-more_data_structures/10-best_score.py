c147fbaffe81500cd2819355c8607ed5b34ffa91
rediet <redietkfle@gmail.com>
2024-04-29 12:12:25 -0700

    Return student name with highest score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if type(a_dictionary) != dict:
-        return None
-    return max(a_dictionary, key=a_dictionary.get)
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
