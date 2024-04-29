5d56ec11650d494b7df8c265cc1f12e24c55bf95
rediet <redietkfle@gmail.com>
2024-04-29 11:29:49 -0700

    Return matrix elements squared

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
+    new_matrix = matrix[:]
     for idx in range(len(new_matrix)):
         new_matrix[idx] = [num * num for num in matrix[idx]]
     return new_matrix
