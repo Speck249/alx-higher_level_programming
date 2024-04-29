6d66f7c2679bf8bf13f9749bfbf96b46eafd96a8
rediet <redietkfle@gmail.com>
2024-04-29 11:33:08 -0700

    Return elements unique to both sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    val_1 = [item for item in set_1 if item not in set_2]
-    val_2 = [item for item in set_2 if item not in set_1]
-    return val_1 + val_2
+    return set_1.symmetric_difference(set_2)
