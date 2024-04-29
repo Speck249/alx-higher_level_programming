dd38090cf6e499701746d9916c41326693982095
rediet <redietkfle@gmail.com>
2024-04-29 11:31:09 -0700

    Replace all occurences of search value

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    new_list = list(map(lambda num: replace if search == num else num, new_list))
-    return new_list
+    new_list = my_list[:]
+    return list(map(lambda num: replace if search == num else num, new_list))
