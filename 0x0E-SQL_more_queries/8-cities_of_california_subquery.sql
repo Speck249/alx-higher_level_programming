d857d02d2ee8f02c2aa154b979bbe4a8f53dd455
rediet <redietkfle@gmail.com>
2024-04-30 05:18:34 -0700

    List all cities in California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,2 @@
--- SQL Query displays common value of two tables via SELECT.
--- Query lists values by id(states) and name(cities).
--- Query uses FOREIGN KEY and PRIMARY KEY.
+-- Query displays all cities in California.
 SELECT id, name FROM cities WHERE state_id=(SELECT id FROM states WHERE name="California" ORDER BY id);
