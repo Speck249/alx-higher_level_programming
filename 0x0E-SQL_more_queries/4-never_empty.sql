91d02b905c07e1ac9b212795e203a76b46cf4b51
rediet <redietkfle@gmail.com>
2024-04-30 05:11:51 -0700

    Create table with attributes where id must not be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,4 @@
--- SQL Query creates tables with two attributes.
--- Query sets default id value to 1.
+-- Query creates new table with id (has default value) and name attributes.
 CREATE TABLE IF NOT EXISTS id_not_null (
 	id INT DEFAULT 1,
 	name VARCHAR(256));
