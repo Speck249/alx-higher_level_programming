489f838394d1b948d34e83b6f67d161b20b753c3
rediet <redietkfle@gmail.com>
2024-04-30 05:06:38 -0700

    Create superuser with root priviledges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- SQL Query creates new user with all privileges.
--- Query will not fail if user already exists.
+-- Query creates new super with root privileges.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
