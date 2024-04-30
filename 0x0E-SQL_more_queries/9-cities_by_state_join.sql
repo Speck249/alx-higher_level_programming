e10142c9797c1fd6a3d7a787af275dc5e215f6a8
rediet <redietkfle@gmail.com>
2024-04-30 05:21:04 -0700

    Display cities in accordance with their respective states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,2 @@
--- SQL Query displays common values from cities table.
--- Query uses INNER JOIN.
+-- Query lists cities and corresponding states.
 SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id;
