67db2df1c399c84eb729b4969209f76b4cf87c07
Rediet <redietkfle@gmail.com>
2024-07-28 22:12:32 -0700

    Function returns sum of two integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
+const args = process.argv;
+let a = Number.parseInt(args[2]);
+let b = Number.parseInt(args[3]);
 
 function add (a, b) {
   return a + b;
