877f44c1b3c7132d178f38f931cdb391a69f1067
Rediet <redietkfle@gmail.com>
2024-07-29 00:30:14 -0700

    Return the second  biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
-const args = process.argv.sort();
-let maxNum, maxIndex;
-maxNum = args[2];
-if (args.length < 4) {
+let args = process.argv.slice(2).map(Number);
+if (args.length === 0) {
   console.log(0);
-} else {
-  for (let i = 3; i <= args.length; i++) {
-    if (args[i] > maxNum) {
-      maxNum = args[i];
-      maxIndex = i;
-    }
-  }
-  console.log(args[maxIndex - 1]);
 }
+args = args.sort((a, b) => (a - b));
+console.log(args[args.length - 2]);
