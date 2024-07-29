c12aebb25202e824911e3d44fb1e17968646cc5c
Rediet <redietkfle@gmail.com>
2024-07-28 21:40:57 -0700

    Print message based on number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-const len = process.argv.length;
-
-if (len == 2) {
+const args = process.argv
+if (args.length === 2) {
   console.log('No argument');
-} else if (len == 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log('Argument found');
 }
