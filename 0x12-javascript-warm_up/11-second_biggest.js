e0387ae622eb761399cb75edfd48483f3931be34
Rediet <redietkfle@gmail.com>
2024-07-29 00:35:53 -0700

    Add conditional for argument length

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-let args = process.argv.slice(2).map(Number);
-if (args.length === 0) {
+if (process.argv.length < 4) {
   console.log(0);
+} else {
+  let args = process.argv.slice(2).map(Number);
+  args.sort((a, b) => (a - b));
+  console.log(args[args.length - 2]);
 }
-args = args.sort((a, b) => (a - b));
-console.log(args[args.length - 2]);
