1f1e5da8d00b2c1c6841991de4f2199d3dcf3d6b
Rediet <redietkfle@gmail.com>
2024-08-16 05:32:10 -0700

    Switch values of width and height

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,32 +2,22 @@
 
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    let rect = '';
-    for (let i = 0; i < this.width; i++) {
-      rect = rect + 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(rect);
+    for (let idx = 0; idx < this.height; idx++) {
+      console.log('X'.repeat(this.width));
     }
   }
 
-  rotate () {
-    let temp = 0;
-    temp = this.width;
+  rotate() {
+    let temp = this.width;
     this.width = this.height;
     this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
   }
 }
 
