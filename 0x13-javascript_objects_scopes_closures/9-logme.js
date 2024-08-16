e271a6f843b5b60fa32fb764b4609124835cb87e
Rediet <redietkfle@gmail.com>
2024-08-16 06:01:36 -0700

    Print argument count and argument value

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let count = 0;
+let index = 0;
 exports.logMe = function (item) {
-  console.log(`${count}: ${item}`);
-  count++;
-};
+  console.log(`${index}: ${item}`);
+  index+=1;
+}
